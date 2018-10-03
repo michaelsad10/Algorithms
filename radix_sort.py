899f3f51fa7457ccb935e7a98a1144b244eb609d
Michael <michaelsad10@gmail.com>
2018-10-03 10:23:42 -0500

    fixed it

--- radix_sort.py
+++ radix_sort.py
@@ -1,14 +1,15 @@
 import math 
 
 def counting_sort(A,x):
-    count = [0] * 9
+    count = [0] * 10
     result = [0] * len(A)
+    exp = pow(10,x) 
     for j in range(0,len(A)):
-        count[int((A[j]/x))%10] += 1
-    for i in range(1,9):
+        count[int((A[j]/exp))%10] += 1
+    for i in range(1,10):
         count[i] += count[i-1]
     for j in range(len(A)-1,-1,-1):
-        key = int((A[j]/x))%10
+        key = int((A[j]/exp))%10
         index = count[key]
         result[index-1] = A[j]
         count[key] -= 1
@@ -16,9 +17,9 @@ def counting_sort(A,x):
 
 def radix_sort(A,n):
     for i in range(0,n):
-       A = counting_sort(A,(pow(10,i)))
+       A = counting_sort(A,i)
     return(A) 
-A = [123,144,123,145,168,123,121]
+A = [123,144,123,145,168,123,121,129]
 x = radix_sort(A,4)
 print(x)
 
